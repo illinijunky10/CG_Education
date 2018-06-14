37cd527acb8324108fc31c295516679679adfef4
Emma <emmakepple@gmail.com>
2018-06-13 20:04:50 -0500

    finshed teacher prototypes and the course prototypes time to start student prototypes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 console.log('This file is linked!');
 
-var teacher_name='Emma Kepple';
-
-var dept_name='Chemistry';
-
-var teacherRatings = [1.5, 4.7, 3.8];
+// var teacher_name='Emma Kepple';
+//
+// var dept_name='Chemistry';
+//
+// var teacherRatings = [1.5, 4.7, 3.8];
 
 // **********************************
 var newRating = parseFloat(window.prompt("We would like for you to review. Please enter a rating between 0.0 - 5.0?"));
@@ -35,19 +35,48 @@ var newRating = parseFloat(window.prompt("We would like for you to review. Pleas
 //             'Avg Rating', getRatingAvg(teacherRatings) );
 // ***************************************
 
-var teacher = {
-  //list and fill properties here
-  name:"Emma Kepple",
-  department:"Chemistry",
-  ratings:[1.5, 4.7, 3.8],
-  addRating:function (newRating) {
+// var teacher = {
+//   //list and fill properties here
+//   name:"Emma Kepple",
+//   department:"Chemistry",
+//   ratings:[1.5, 4.7, 3.8],
+//   addRating:function (newRating) {
+//     if (0 <= newRating && newRating <= 5 ) {
+//       this.ratings.push(newRating);
+//     } else {
+//       window.prompt("We would like for you to review. Please enter a rating between 0.0 - 5.0!");
+//     }
+//   },
+//   getAvgRating: function (ratings) {
+//     var sum = 0;
+//      for (var i = 0; i < this.ratings.length; i++){
+//          sum = sum + this.ratings[i];
+//      }
+//      var avg = sum / this.ratings.length;
+//      alert("Thanks for your review! "+ this.name + "'s average rating is now "+ avg);
+//      return avg;
+//    }
+// };
+// console.log(teacher.ratings);
+// console.log(teacher.addRating(newRating));
+// console.log(teacher.ratings);
+// console.log(teacher.getAvgRating());
+
+function Teacher (teacher_name, teacher_department, ratings){
+  this.name = teacher_name;
+  this.department= teacher_department;
+  this.ratings= ratings;
+}
+
+Teacher.prototype = {
+  addRating: function (newRating) {
     if (0 <= newRating && newRating <= 5 ) {
       this.ratings.push(newRating);
     } else {
       window.prompt("We would like for you to review. Please enter a rating between 0.0 - 5.0!");
     }
   },
-  getAvgRating: function (ratings) {
+  getAvgRating: function () {
     var sum = 0;
      for (var i = 0; i < this.ratings.length; i++){
          sum = sum + this.ratings[i];
@@ -57,10 +86,34 @@ var teacher = {
      return avg;
    }
 };
-console.log(teacher.ratings);
-console.log(teacher.addRating(newRating));
-console.log(teacher.ratings);
-console.log(teacher.getAvgRating());
+
+// var mathTeacher = new Teacher("Kent", "Math", [3.2, 5.0, 4.5]);
+//
+// mathTeacher.name
+// mathTeacher.addRating(3.7);
+//
+// var spanishTeacher = new Teacher("Gallo", "Language Arts", [1.0]);
+// spanishTeacher.addRating(0.5);
+
+var physicsTeacher = new Teacher("Ben Kenobi", "Physics", [4.7, 5.0, 1.0]);
+physicsTeacher.addRating(5.0);
+console.log(physicsTeacher);
+console.log(physicsTeacher.ratings);
+console.log(physicsTeacher.getAvgRating());
+
+var psychTeacher = new Teacher("Janice Juraska", "Psychology", [3.5, 5.0, 0.5]);
+psychTeacher.addRating(5.0);
+console.log(psychTeacher);
+console.log(psychTeacher.ratings);
+console.log(physicsTeacher.getAvgRating());
+
+var historyTeacher = new Teacher ("Emily Roberts", "History", [5.0, 2.7, 1.0]);
+historyTeacher.addRating(4.7);
+console.log(historyTeacher);
+console.log(historyTeacher.ratings);
+console.log(historyTeacher.getAvgRating());
+
+// Student Section
 
 var student_name='Megan Trainor';
 
@@ -142,53 +195,89 @@ function welcomeStudentsByGraduatingClass (gradYear, welcomeMessage) {
 //
 // ]
 
-var course = {
-  name: 'Astronomy',
-  department: 'Physics',
-  teacher: teacher,
-  semester: 'Fall 2018'
-};
- var course2 = {
-   name: 'Personality',
-   department: 'Psychology',
-   teacher: teacher,
-   semester: 'Fall 2018'
- };
- var course3 = {
-   name: 'Force',
-   department: 'Physics',
-   teacher: teacher,
-   semester: 'Spring 2019'
- };
- var course4 = {
-   name: 'Abnormal Psychology',
-   department: 'Psychology',
-   teacher: teacher,
-   semester: 'Spring 2019'
- };
-var courses = [course, course2, course3, course4];
-
+// var course = {
+//   name: 'Astronomy',
+//   department: 'Physics',
+//   teacher: physicsTeacher,
+//   semester: 'Fall 2018'
+// };
+ // var course2 = {
+ //   name: 'Personality',
+ //   department: 'Psychology',
+ //   teacher: psychTeacher,
+ //   semester: 'Fall 2018'
+ // };
+ // var course3 = {
+ //   name: 'Force',
+ //   department: 'Physics',
+ //   teacher: physicsTeacher,
+ //   semester: 'Spring 2019'
+ // };
+ // var course4 = {
+ //   name: 'Abnormal Psychology',
+ //   department: 'Psychology',
+ //   teacher: psychTeacher,
+ //   semester: 'Spring 2019'
+ // };
+ //
+// var courses = [course, course2, course3, course4];
+//
 var inputDepartment = window.prompt("What department is the course in?");
+//
+// function filterByDepartment (inputDepartment, courses) {
+//   var matchingCourses = [];
+//
+//   for (var i=0; i < courses.length; i++) {
+//     var aCourse = courses[i];
+//     if (aCourse.department == inputDepartment){
+//       // matchingCourses.push(aCourse);
+//      alert(matchingCourses += aCourse.name + ', ');
+//     }
+//   }
+//
+//   return matchingCourses;
+// }
 
-function filterByDepartment (inputDepartment, courses) {
-  var matchingCourses = [];
+// filterByDepartment (inputDepartment, courses)
+// var result = filterByDepartment (inputDepartment, courses);
+//
+// console.log(result);
 
-  for (var i=0; i < courses.length; i++) {
-    var aCourse = courses[i];
-    if (aCourse.department == inputDepartment){
-      matchingCourses.push(aCourse);
-     //matchingCourses += aCourse.name + ', ';
-    }
-  }
-
-  return matchingCourses;
+function Course (courseName, courseDepartment, teacher, semester){
+  this.name = courseName;
+  this.department = courseDepartment;
+  this.teacher = teacher;
+  this.semester = semester;
 }
 
-filterByDepartment (inputDepartment, courses)
-var result = filterByDepartment (inputDepartment, courses);
-console.log(result);
+Course.prototype = {
+  filterByDepartment: function (inputDepartment, courses) {
+    var matchingCourses = [];
 
-console.log(course4.teacher.name);
+    for (var i=0; i < courses.length; i++) {
+      var aCourse = courses[i];
+      if (aCourse.department == inputDepartment){
+        // matchingCourses.push(aCourse);
+       matchingCourses += aCourse.name + ', ';
+      }
+    }
+    alert(matchingCourses);
+    return matchingCourses;
+  }
+}
+
+var Astronomy = new Course ("Astronomy", "Physics", physicsTeacher, "Fall 2018");
+
+var Personality = new Course ("Personality", "Psychology", psychTeacher, "Fall 2018");
+
+var Force = new Course ("Force", "Physics", physicsTeacher, "Spring 2019");
+
+var Abnormal = new Course ("Abnormal Psychology", "Psychology", psychTeacher, "Spring 2019");
+
+var courses = [Astronomy, Personality, Force, Abnormal];
+
+Astronomy.filterByDepartment("Physics", courses);
+
 // Real Deal Here
 
 // var inputDepartment = window.prompt("What department is the course in?");
